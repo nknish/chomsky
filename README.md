32f9e9a1856bdff35fc18c8324f011d5feb2d036
nknish <noahknish@gmail.com>
2025-03-28 13:55:52 -0400

    make installer more robust, ditch makefile

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-# chomsky - a *meaningless* command line tool
+# chomsky - a _meaningless_ command line tool
+
 for those times when you want to say nothing, but don't know how to say it
 
 ## description
+
 - a python command line tool that generates a syntactically valid but semantically nonsensical sentence.
-- sentences will loosely follow the phrase structure of ["colorless green ideas sleep furiously"](https://en.wikipedia.org/wiki/Colorless_green_ideas_sleep_furiously) 
-- that's about it 
+- sentences will loosely follow the phrase structure of ["colorless green ideas sleep furiously"](https://en.wikipedia.org/wiki/Colorless_green_ideas_sleep_furiously)
+- that's about it
 
 ## installation
 
 ### dependencies
+
 - only supports **mac** os.
 - you've gotta already have **python** installed
 - downloading this without using `git clone` will be a headache but is certainly possible
@@ -18,14 +21,15 @@ for those times when you want to say nothing, but don't know how to say it
 - just run
 
 ```
-git clone https://github.com/nknish/chomsky; make -C chomsky install
+git clone https://github.com/nknish/chomsky && sh chomsky/install.sh
 ```
 
 - if you have permission issues, you'll be prompted to enter your password.
 
-- and then the command `chomsky` will be in your system path (in `usr/local/bin`). 
+- and then the command `chomsky` will be in your system path (in `usr/local/bin`).
 
 ## usage
+
 - `chomsky` to print a sentence to stdout
 - `chomsky --color` or `chomsky -c` to make it pretty
 - `chomsky 3` (or any positive integer) to get that many sentences
@@ -33,15 +37,18 @@ git clone https://github.com/nknish/chomsky; make -C chomsky install
 - i might add some more features later but who knows
 
 ## tech stack
+
 - written in python
-- i figured out how to use a makefile and shell scripting to make installation easy, which was cool
-    - shoutout jarrett's cs449
- 
+- i figured out how to use shell scripting to make installation easy, which was cool
+  - shoutout jarrett's cs449
+
 ## contributing
+
 - if you have any ideas or find any bugs, just let me know
 - if you get a really great sentence, you should also share that
 
 ## credits
+
 - credit to noam chomsky i guess
-- somewhat inspired by [fortune](https://en.wikipedia.org/wiki/Fortune_(Unix))
+- somewhat inspired by [fortune](<https://en.wikipedia.org/wiki/Fortune_(Unix)>)
 - lists of words from [here](http://www.ashley-bovan.co.uk/words/partsofspeech.html) for now.
--- install.sh
+++ install.sh
@@ -1,28 +1,94 @@
 #!/bin/bash
 
-# polite introduction
-echo "installing chomsky..."
+# take some precautions
+set -e
+set -o pipefail
 
-# check/adjust permissions
-if [ ! -w /usr/local/bin ]; then
-    echo "oops, i can't write to /usr/local/bin. enter password to change permissions"
-    sudo chmod u+w /usr/local /usr/local/bin /usr/local/share
+# get some filepaths
+TARGET_DIR="/usr/local/bin"
+R_TARGET_DIR="/usr/local/share/chomsky"
+EXE="chomsky"
+R_SRC_DIR="words"
+ORIGINAL_DIR=$(pwd)
+
+# polite introduction
+echo "
+*******************************************
+*******************************************
+******** chomsky installer running ********    
+*******************************************
+*******************************************
+"
+
+# check for python
+echo "1. checking for dependencies"
+if ! command -v python3 &>/dev/null; then
+    echo "you don't have python. this isn't going to work out..."
+    exit 1
 fi
 
-# move everything to the right place
-cp chomsky /usr/local/bin
-mkdir -p /usr/local/share/chomsky
-cp -r words /usr/local/share/chomsky
-chmod +x /usr/local/bin/chomsky
+# check for script, executable, resources
+echo "2. checking for files"
+if [ ! -d "chomsky" ]; then
+    echo "script can't find itself? there's no coming back from that!"
+    exit 1
+fi
+cd chomsky;
 
-# delete everything safely
+if [ ! -f "$EXE" ]; then
+    echo "script can't find executable, exiting"
+    exit 1
+fi
+if [ ! -d "$R_SRC_DIR" ]; then
+    echo "script can't find word lists, exiting"
+    exit 1
+fi
+
+# check/adjust sudo status
+echo "3. checking for permissions"
+if [ ! -w "$TARGET_DIR" ] || ([ ! -d "$R_TARGET_DIR" ] && [ ! -w "$(dirname "$R_TARGET_DIR")" ]); then
+    echo "oops, i can't access to '$TARGET_DIR' or '$R_TARGET_DIR'. enter password to change permissions"
+    sudo -v || {
+        echo "no permission means no fun :("
+        exit 1
+    }
+fi
+
+# make target directories if necessary (but first check if resources dir exists)
+echo "4. creating new directories (if necessary)"
+R_EXISTS=false
+if [ -d "$R_TARGET_DIR/$R_SRC_DIR" ]; then
+    R_EXISTS=true
+fi
+mkdir -p "$TARGET_DIR"
+mkdir -p "$R_TARGET_DIR"
+
+# check if overwriting
+echo "5. checking for an existing version"
+if [ -f "$TARGET_DIR/$EXE" ] || [ "$R_EXISTS" = true ]; then
+    echo "wait a sec. some of the files we're creating already exist. we'll have to overwrite them."
+    read -p "keep going anyways? (y/n): " confirm
+    if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+        echo "okay then, i guess this is it..."
+        exit 1
+    fi
+fi
+
+# move script and make it executable
+echo "6. moving script and resources"
+cp "$EXE" "$TARGET_DIR/$EXE"
+chmod +x "$TARGET_DIR/$EXE"
+cp -r "$R_SRC_DIR" "$R_TARGET_DIR"
+
+# delete unnecessary files
 if [ "$(basename "$(pwd)")" = "chomsky" ]; then
-    echo "deleting installer directory at $(pwd)..."
+    echo "7. cleaning up installation files in $(pwd)"
     rm -rf "$(pwd)"
 else
     echo "error: could not locate and delete installer directory"
 fi
 
 # finish up
-cd ~
-echo "successfully installed! try the command 'chomsky' or 'chomsky 3' to get started."
+cd "$ORIGINAL_DIR"
+echo "
+successfully installed! try 'chomsky' or 'chomsky 3' to get started"
